b3237504f60a085f5cc550b1930ac264ec1d31eb
Aidana Sharip <aidanasharip0797@gmail.com>
2024-03-18 17:52:46 -0500

    committed

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "us-east-2"  
+}
+
+resource "aws_vpc" "test" {
+  cidr_block       = "10.0.0.0/20"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-022661f8a4a1b91cf"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "Test-Instance"
+  }
+}
